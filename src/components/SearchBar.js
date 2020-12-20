c4d3ac404e50725f97a6e571e549b8b7b1b00fc7
Lakshmikanth Chippa <l.chippa@3dqr.de>
2020-12-20 23:35:05 +0100

    started videokit App

---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div>searchBar...</div>
+        );
+    }
+}
+
+export default SearchBar;
