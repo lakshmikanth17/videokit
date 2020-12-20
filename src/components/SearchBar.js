bdb36b13102d85f6d73bdd176426dd0ce2a4f380
Lakshmikanth Chippa <l.chippa@3dqr.de>
2020-12-21 00:06:24 +0100

    added event handlers and controlled input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,35 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+    //initialize state object and pass this state to input as a value
+    state = {VkitSearch: ''};
+
+    //eventHandler fn
+    //alternative to inline fn as :
+    // onChange{e => this.setState({VkitSearch: e.target.value})}
+    onInputChange = (event) => {
+        this.setState({VkitSearch: event.target.value })
+    };
+
+    onFormSubmit = event => {
+        //to stop from sending req to backend(default action)
+        event.preventDefault();
+
+        //TODO: make sure a callback from parent Component
+    }
+
+
     render() {
         return (
             <div className="search-bar ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Search VideoKit</label>
-                        <input type="text" />
+                        <input
+                            type="text"
+                            value={this.state.VkitSearch}
+                            onChange={this.onInputChange}
+                        />
                     </div>
                 </form>
             </div>
